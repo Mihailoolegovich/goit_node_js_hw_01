588a09b33ce853e5e3f825a51a141e097e308a98
Mihailoolegovich <mihailoolegovich@gmail.com>
2022-05-21 10:01:09 +0300

    work version

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # goit_node_js_hw_01
+
+## Screenshots of console commands:
+
+[List](https://monosnap.com/file/CXWJRenWJC5hwiUPCWRRI2pE8i3cWf);
+
+[Get](https://monosnap.com/file/LDgcGrs5YQ0iH1Uj8LZXfl06cza9mb);
+
+[Add](https://monosnap.com/file/nwiNHutp0PNnvyQUlitDX1oO3uT5od);
+
+[Remove](https://monosnap.com/file/mAHNl6xOufTELsODaONwdIzZknFwkU);
---
+++ contacts.js
@@ -0,0 +1,48 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { v4 } = require("uuid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contactList = JSON.parse(data);
+  return contactList;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((el) => el.id === contactId);
+  if (!result) {
+    return null;
+  }
+  return result;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: v4(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+
+  return newContact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const removedContact = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+
+  return removedContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
